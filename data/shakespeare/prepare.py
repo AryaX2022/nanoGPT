bc7c4ca9b73f31e4311e45f2c1bb4153ed02225b
AryaX2022 <6983299@gmail.com>
2023-05-31 20:35:00 +0800

    Update prepare.py

--- data/shakespeare/prepare.py
+++ data/shakespeare/prepare.py
@@ -6,7 +6,8 @@ import numpy as np
 # download the tiny shakespeare dataset
 input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
 if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
+    # data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
+    data_url = 'https://raw.githubusercontent.com/AryaX2022/nanoGPT/master/data/shakespeare/book.txt'
     with open(input_file_path, 'w') as f:
         f.write(requests.get(data_url).text)
 
